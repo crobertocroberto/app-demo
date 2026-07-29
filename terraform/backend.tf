26c0584e6d6d80a194c10af5ddb6ac9458ad4611
CR <croberto@3htp.com>
2026-07-29 14:39:05 -0500

    ajuste en state

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket = "3htp-vault-demos"
-    key    = "app-demo/terraform.tfstate"
-    region = "us-east-1"
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
